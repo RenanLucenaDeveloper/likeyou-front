f3f2e92566fa564dd07578250aa0518ead62be2c
Guilherme Padial <guilherme.padial4@gmail.com>
2024-12-03 15:28:36 -0300

    Atualiza layout da tela welcome: adiciona fundo no desktop, ajusta responsividade e estilo do conteúdo

--- src/components/welcome/index.jsx
+++ src/components/welcome/index.jsx
@@ -1,25 +1,36 @@
-import { FaArrowRight } from "react-icons/fa";
+import { useState } from "react";
 
-import SmileLikeYou from "./assets/img/smile-likeyou.png";
+import personImage from "./assets/img/person.svg";
 
 import "./styles/global.scss";
 import "./styles/styles.scss";
 
 export function Welcome() {
-  return (
-    <div className="container__main">
-      <div className="container__content">
-        <img src={SmileLikeYou} alt="" />
-        <div className="title__actions">
-          <div className="content__title">
-            <h1>Vamos nos conectar em uma plataforma</h1>
-          </div>
+  const [activeButton, setActiveButton] = useState(0);
 
-          <button className="btn__connect">
-            Conectar <FaArrowRight size={16} />
-          </button>
-        </div>
+  const handleMouseEnter = (index) => {
+    setActiveButton(index);
+  };
+
+  return (
+    <main className="container__main">
+      <img src={personImage} alt="Smile face" />
+
+      <div className="container__content">
+        <h1>Entre e comece a avaliar!</h1>
+        <p>
+          Crie sua conta ou faça login para começar a avaliar as pessoas ao seu redor. Compartilhe suas opiniões e descubra o que outros
+          pensam sobre você!
+        </p>
       </div>
-    </div>
+
+      <div className="container__btns">
+        {["Entrar", "Cadastrar"].map((text, index) => (
+          <button key={index} className={`btn ${activeButton === index ? "active" : ""}`} onMouseEnter={() => handleMouseEnter(index)}>
+            {text}
+          </button>
+        ))}
+      </div>
+    </main>
   );
 }
