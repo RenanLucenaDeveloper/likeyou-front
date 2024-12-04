b21753d72296b4f5eb912cf26ddc0314ea2c4b56
RenanLucenaDeveloper <Reenanluceena@gmail.com>
2024-12-03 21:21:51 -0300

    404

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/likeyou-front",
+  base: "/likeyou-front/",
   plugins: [react()],
   css: {
     preprocessorOptions: {
